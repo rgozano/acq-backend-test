5d082875c7404b67dc85dd1bd49c359a824d8233
Romel Gozano <romel.gozano@gmail.com>
2018-09-03 09:11:51 +0800

    Add db config

--- config/db.js
+++ config/db.js
@@ -1,3 +1,9 @@
-module.exports = {
-	url : "mongodb://localhost:27017/project"
+var db = "mongodb://localhost:27017/project";
+if (process.env.PORT) {
+	db  = "mongodb://romelgozano:123pass@ds141902.mlab.com:41902/project";
 }
+
+module.exports = {
+	url : db
+}
+
